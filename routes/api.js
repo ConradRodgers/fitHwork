65502c4526e81b6ab5e01464227aa78ba576eced
Conrad Rodgers <conrad13500@gmail.com>
2020-05-18 18:10:34 -0700

    stats route fix

--- routes/api.js
+++ routes/api.js
@@ -21,4 +21,16 @@ router.put("/api/workouts/:id", (req, res) => {
   });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkouts) => {
+      console.log(dbWorkouts);
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
